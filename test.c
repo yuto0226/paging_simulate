7ff097e872ae75df1a0af6201ee9a35f67a9d138
yuto0226 <wayne71112@gmail.com>
2025-01-02 15:26:25 +0800

    fix: 修正 page test

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+// 直接引入 main.c 然後呼叫測試函式使用即可
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,6 +20,10 @@
 )
 
 void pa_test() {
+    printf("\n##################################\n");
+    printf("#      physica address test      #\n");
+    printf("##################################\n");
+    printf("測試實體憶體的分配\n\n");
     void *p = kalloc();
     *(uint64 *)p = 0xdeadbeef;
     info("p(%p) = 0x%lx\n", p, *(uint64 *)p);
@@ -27,6 +32,11 @@ void pa_test() {
 }
 
 void va_test() {
+    printf("\n##################################\n");
+    printf("#      virtual address test      #\n");
+    printf("##################################\n");
+    printf("測試虛擬記憶體的建立與分配\n\n");
+
     pagetable_t pagetable = uvmcreate();
     if (pagetable == 0) {
         failed("uvmcreate()");
@@ -54,45 +64,91 @@ void va_test() {
 }
 
 void page_test() {
-    uint64 *data = kalloc();
-    uint64 offset = (uint64)data & 0x0fff;
-    info("data=0x%lx, offset=0x%lx\n", (uint64)data, offset);
+    printf("\n##################################\n");
+    printf("#           page test            #\n");
+    printf("##################################\n");
+    printf("測試 1 個 frame 的情況下重複 page in, page out.\n\n");
 
+    swap_init();
     pagetable_t pagetable = uvmcreate();
-    if (pagetable == 0) {
-        failed("uvmcreate()");
-        return;
-    }
+
+    // create page 0
+    uint64 *data = kalloc();
+    memset(data, 'A', PGSIZE);
+    strcpy((char*)data, "This data from section A.\n");
+    info("data=0x%lx\n", (uint64)data);
 
     uint64 va = 0x1000;
 
-    mappages(pagetable, va, PGSIZE, (uint64)data, PTE_U | PTE_R | PTE_W, 1);
+    mappages(pagetable, va, PGSIZE, (uint64)data, PTE_U | PTE_R | PTE_W);
     pte_t *pte = walk(pagetable, va, 0);
     pte_info(pte);
 
-    uint64 addr = walkaddr(pagetable, va);
-    info("addr=0x%lx\n", addr);
+    // create page 1 with swap space
+    uint64* data1 = kalloc();
+    memset(data1, 'B', PGSIZE);
+    strcpy((char*)data1, "This data from section B.\n");
+    info("data1=0x%lx\n", (uint64) data1);
 
-    if(PTE2PA(*pte) != addr) {
-        failed("walkaddr()");
-        return;
-    }
+    uint64 va1 = 0x2000;
+    int si = swap_out(data1);
+    mappages(pagetable, va1, PGSIZE, si, PTE_U | PTE_R | PTE_W | PTE_S);
+    pte_t* pte1 = walk(pagetable, va1, 0);
+    pte_info(pte1);
 
+    // page out page 0
+    printf("\n");
+    warn(COLOR_BOLD_YELLOW"PAGE OUT\n\n"COLOR_RESET);
     page_out(pagetable, va);
-    if ((*pte & PTE_V) != 0) {
+    if((*pte & PTE_S) == 0) {
         failed("page_out()");
         return;
+    } else {
+        passed("page_out()");
     }
 
     pte_info(pte);
 
-    page_in(pagetable, va);
-    if ((*pte & PTE_V) == 0) {
+    // page in page 1
+    printf("\n");
+    warn(COLOR_BOLD_YELLOW"PAGE IN\n\n"COLOR_RESET);
+    page_in(pagetable, va1);
+    if ((*pte1 & PTE_S) != 0) {
         failed("page_in()");
         return;
+    } else {
+        passed("page_in()");
+    }
+
+    pte_info(pte1);
+    printf("page 2 data: %s\n", (char*)PTE2PA(*pte1));
+
+    // page  out page 1
+    printf("\n");
+    warn(COLOR_BOLD_YELLOW"PAGE OUT\n\n"COLOR_RESET);
+    page_out(pagetable, va1);
+    if((*pte1 & PTE_S) == 0) {
+        failed("page_out()");
+        return;
+    } else {
+        passed("page_out()");
+    }
+
+    pte_info(pte1);
+
+    // swap in page 0
+    printf("\n");
+    warn(COLOR_BOLD_YELLOW"PAGE OUT\n\n"COLOR_RESET);
+    page_in(pagetable, va);
+    if((*pte & PTE_S) != 0) {
+        failed("page_in()");
+        return;
+    } else {
+        passed("page_in()");
     }
 
     pte_info(pte);
+    printf("page 2 data: %s\n", (char*) PTE2PA(*pte));
 
     passed("page_out() and page_in()");
 }
