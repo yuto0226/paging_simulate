f851c3f8eca1c3d623ef02689ecda9842d1a7bd0
yuto0226 <wayne71112@gmail.com>
2025-01-02 15:25:53 +0800

    feat: 增加 pte_info 顯示的資訊

--- debug.c
+++ debug.c
@@ -10,16 +10,29 @@ panic(char* s) {
 
 void pte_info(pte_t *pte) {
   info("pte info: 0x%lx\n", *pte);
+  if(PTE_FLAGS(*pte) & PTE_S) {
+    info("this page is swapped\n");
+  }
   printf("--------------------------------\n");
-  printf("frame addr      : 0x%lx\n", PTE2PA(*pte));
-  printf("Valid           : %s\n", (*pte & PTE_V) ? "Yes" : "No");
-  printf("Readable        : %s\n", (*pte & PTE_R) ? "Yes" : "No");
-  printf("Writable        : %s\n", (*pte & PTE_W) ? "Yes" : "No");
-  printf("Executable      : %s\n", (*pte & PTE_X) ? "Yes" : "No");
-  printf("User Accessible : %s\n", (*pte & PTE_U) ? "Yes" : "No");
-  printf("Global          : %s\n", (*pte & PTE_G) ? "Yes" : "No");
-  printf("Accessed        : %s\n", (*pte & PTE_A) ? "Yes" : "No");
-  printf("Dirty           : %s\n", (*pte & PTE_D) ? "Yes" : "No");
-  printf("Swapped         : %s\n", (*pte & PTE_S) ? "Yes" : "No");
+  printf("       addr        flags\n");
+  printf("+----------------+-------+\n");
+  if(PTE_FLAGS(*pte) & PTE_S)
+    printf("| 0x%012lx | 0x%03lx |\n", *pte >> 10,  PTE_FLAGS(*pte));
+  else
+    printf("| 0x%012lx | 0x%03lx |\n", PTE2PA(*pte), PTE_FLAGS(*pte));
+  printf("+----------------+-------+\n");
+  if(PTE_FLAGS(*pte) & PTE_S)
+    printf("swap index      : 0x%lx\n", *pte >> 10);
+  else
+    printf("frame addr      : 0x%lx\n", PTE2PA(*pte));
+  printf("Valid           : %s\n", (*pte & PTE_V) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("Readable        : %s\n", (*pte & PTE_R) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("Writable        : %s\n", (*pte & PTE_W) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("Executable      : %s\n", (*pte & PTE_X) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("User Accessible : %s\n", (*pte & PTE_U) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("Global          : %s\n", (*pte & PTE_G) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("Accessed        : %s\n", (*pte & PTE_A) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("Dirty           : %s\n", (*pte & PTE_D) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
+  printf("Swapped         : %s\n", (*pte & PTE_S) ? COLOR_BOLD_GREEN"Yes"COLOR_RESET : "No");
   printf("--------------------------------\n");
 }
